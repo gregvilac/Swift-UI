ca178ecf3f8a652b2ec13e76ce6afb620ccd1652
Greg Vilac <gregvilac@hotmail.com>
2024-10-02 12:43:04 -0700

    Initial Commit

---
+++ Testing Application NoDataNoTests/Testing_Application_NoDataNoTestsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Testing_Application_NoDataNoTestsApp.swift
+//  Testing Application NoDataNoTests
+//
+//  Created by Greg Vilac on 2024-10-02.
+//
+
+import SwiftUI
+
+@main
+struct Testing_Application_NoDataNoTestsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
